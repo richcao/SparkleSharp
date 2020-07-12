657db47ae99e86d708f4d0c808b5c1720311dba9
Richard Cao <richcao@Richards-MBP-3.localdomain>
2020-07-12 10:19:06 -0700

    Fixes.

--- Sparkle/build_framework.sh
+++ Sparkle/build_framework.sh
@@ -4,4 +4,6 @@ DIR=$(dirname ${0})
 cd ${DIR}
 git submodule init && git submodule update
 cd 3rdparty/Sparkle
-xcodebuild -configuration Release -target Sparkle
+git submodule init && git submodule update
+# xcodebuild -configuration Release -target Sparkle
+xcodebuild -scheme Distribution -configuration Release -derivedDataPath build
